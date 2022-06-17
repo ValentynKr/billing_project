acbfc95c348f441eaa82a519e6619661e1d3849f
Valentine <krilovetsky@gmail.com>
2022-06-17 16:46:49 +0300

    Added ReportWriter as interface implementation

--- src/main/java/com/epam/billing/controller/RegistrationServlet.java
+++ src/main/java/com/epam/billing/controller/RegistrationServlet.java
@@ -13,6 +13,11 @@ import java.io.IOException;
 
 @WebServlet(urlPatterns = {"/registration"})
 public class RegistrationServlet extends HttpServlet {
+    public static final String CONFIRMATION_DON_T_MATCH = "Password and confirmation don`t match";
+    public static final String SUCH_EMAIL_IS_ALREADY_REGISTERED = "User with such email is already registered";
+    public static final String REGISTRATION_ACCOMPLISHED_MESSAGE = "Registration was accomplished. Thank you!";
+    public static final String PASS_INVALID_MESSAGE = "Chosen pass is invalid. It should contain at least 8 symbols, including number, letter and special symbol";
+    public static final String EMAIL_INVALID_MESSAGE = "Chosen email is invalid. It should`n contain special symbols or cyrillic letters. Example - ggg@gmail.com";
     private UserService userService;
 
 
@@ -26,27 +31,36 @@ public class RegistrationServlet extends HttpServlet {
 
         String password = req.getParameter("password");
         String confPassword = req.getParameter("confPassword");
+        String email = req.getParameter("email");
+        String name = req.getParameter("name");
+
         if (!password.equals(confPassword)) {
-            req.getSession().setAttribute("Alert", "Password and confirmation don`t match");
+            req.getSession().setAttribute("Alert", CONFIRMATION_DON_T_MATCH);
         } else {
-            if (ValidationUtil.isEmailValid(req.getParameter("email"))) { //toDo need to be refactored
-                String email = req.getParameter("email");
+            if (ValidationUtil.isEmailValid(email)) {
                 if (userService.getByEmail(email).isPresent()) {
-                    req.getSession().setAttribute("Alert", "User with such email is already registered"); //toDo should be specified by locale
+                    req.getSession().setAttribute("Alert", SUCH_EMAIL_IS_ALREADY_REGISTERED);
                 } else {
-                    if (ValidationUtil.isPasswordValid(req.getParameter("password"))) {
-                        String name = req.getParameter("name");
-                        User user = new User().setName(name).setEmail(email).setAdmin(false).setPassword(PasswordHashingUtil.getSaltedHash(password));
-                        userService.save(user);
-                        req.getSession().setAttribute("Alert", "Registration was accomplished. Thank you!");
+                    if (ValidationUtil.isPasswordValid(password)) {
+                        createUser(password, email, name);
+                        req.getSession().setAttribute("Alert", REGISTRATION_ACCOMPLISHED_MESSAGE);
                     } else {
-                        req.getSession().setAttribute("Alert", "Chosen pass is invalid. It should contain at least 8 symbols, including number, letter and special symbol");
+                        req.getSession().setAttribute("Alert", PASS_INVALID_MESSAGE);
                     }
                 }
             } else {
-                req.getSession().setAttribute("Alert", "Chosen email is invalid. It should`n contain special symbols or cyrillic letters. Example - ggg@gmail.com");
+                req.getSession().setAttribute("Alert", EMAIL_INVALID_MESSAGE);
             }
         }
         req.getRequestDispatcher("/jsp/registration.jsp").forward(req, resp);
+    }
+
+    private void createUser(String password, String email, String name) {
+        User user = new User()
+                .setName(name)
+                .setEmail(email)
+                .setAdmin(false)
+                .setPassword(PasswordHashingUtil.getSaltedHash(password));
+        userService.save(user);
     }
 }
